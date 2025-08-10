1a2ae6688283b1d62b85773463500afa99ec01ea
se-haru <rikiharuyama3@gmail.com>
2025-08-10 15:55:15 +0900

    SearchServiceに、Mapperに値を渡すメソッドを追加

--- src/main/java/com/example/demo/service/SearchService.java
+++ src/main/java/com/example/demo/service/SearchService.java
@@ -1,15 +1,33 @@
 package com.example.demo.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.MergedAnnotations.Search;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.mapper.SearchMapper;
 
 @Service
 public class SearchService {
+    @Autowired
+    private SearchMapper mapper;
 
-	@Autowired
-	private SearchMapper mapper;
-	
-	
+    public List<Search> search(int id, String name,
+                                 Integer minAge, Integer maxAge,
+                                 LocalDate startL, LocalDate startR,
+                                 LocalDate endL, LocalDate endR) {
+        String sL = startL != null ? startL.toString() : null;
+        String sR = startR != null ? startR.toString() : null;
+        String eL = endL != null ? endL.toString() : null;
+        String eR = endR != null ? endR.toString() : null;
+      //値がNullじゃなければ文字列にして、NullならそのままNullにして検索条件に含めない
+        return mapper.search(id, name, minAge, maxAge, sL, sR, eL, eR);
+    }
+
+//    public void deleteByIds(List<Long> ids) {
+//        if (ids != null && !ids.isEmpty()) mapper.deleteByIds(ids);
+//    }
 }
+
