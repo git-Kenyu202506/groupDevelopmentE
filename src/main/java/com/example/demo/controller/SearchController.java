6d6f75c220dbe389f7a3b055a21e443be07e3709
se-haru <rikiharuyama3@gmail.com>
2025-08-10 16:09:01 +0900

    コントローラーに社員検索機能を追加

--- src/main/java/com/example/demo/controller/SearchController.java
+++ src/main/java/com/example/demo/controller/SearchController.java
@@ -1,7 +1,15 @@
 package com.example.demo.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.MergedAnnotations.Search;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.service.SearchService;
 
@@ -10,6 +18,30 @@ public class SearchController {
 	
 	@Autowired
 	private SearchService service;//サービスクラスをもってきてる
-	
+	@PostMapping("/search")
+    public String search(
+        @RequestParam(required=false) int id,
+        @RequestParam(required=false) String name,
+        @RequestParam(required=false) Integer minAge,
+        @RequestParam(required=false) Integer maxAge,
+        @RequestParam(required=false) @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate startL,
+        @RequestParam(required=false) @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate startR,
+        @RequestParam(required=false) @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate endL,
+        @RequestParam(required=false) @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate endR,
+        Model model) {
 
+        List<Search> results = service.search(id, name, minAge, maxAge, startL, startR, endL, endR);
+
+        model.addAttribute("search", results);
+        model.addAttribute("id", id);
+        model.addAttribute("name", name);
+        model.addAttribute("minAge", minAge);
+        model.addAttribute("maxAge", maxAge);
+        model.addAttribute("startL", startL);
+        model.addAttribute("startR", startR);
+        model.addAttribute("endL", endL);
+        model.addAttribute("endR", endR);
+        return "search";
+	}
+	
 }
