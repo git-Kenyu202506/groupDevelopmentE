5df8ba9311bbf62f513c8a42edd535e8ccceddf4
se-haru <rikiharuyama3@gmail.com>
2025-08-10 15:35:53 +0900

    searchMapperに社員情報検索を追加

--- src/main/java/com/example/demo/mapper/SearchMapper.java
+++ src/main/java/com/example/demo/mapper/SearchMapper.java
@@ -1,8 +1,33 @@
 package com.example.demo.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.springframework.core.annotation.MergedAnnotations.Search;
 
 @Mapper
 public interface SearchMapper {
+	//社員情報検索
+    @SelectProvider(type=SearchSqlProvider.class, method="buildSearchSql")
+    @Result(property = "startDate", column = "start_date")// マッピングする！
+    @Result(property = "endDate", column = "end_date")// マッピングする！
+    List<Search> search(@Param("id") int id,
+                          @Param("name") String name,
+                          @Param("minAge") Integer minAge,
+                          @Param("maxAge") Integer maxAge,
+                          @Param("startL") String startL,
+                          @Param("startR") String startR,
+                          @Param("endL") String endL,
+                          @Param("endR") String endR);
 
+//    @Delete({"<script>",
+//             "DELETE FROM employee WHERE id IN",
+//             "<foreach collection='ids' item='id' open='(' separator=',' close=')'>",
+//             "#{id}",
+//             "</foreach>",
+//             "</script>"})
+//    void deleteByIds(@Param("ids") List<Long> ids);
 }
