0729f1f26324be2d06f287c21ef40bc2db5f8277
se-haru <rikiharuyama3@gmail.com>
2025-08-11 14:40:51 +0900

    Mapperで自ページ内で削除する機能になっていたので、delete機能を削除

--- src/main/java/com/example/demo/mapper/SearchMapper.java
+++ src/main/java/com/example/demo/mapper/SearchMapper.java
@@ -2,7 +2,6 @@ package com.example.demo.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Result;
@@ -28,11 +27,4 @@ public interface SearchMapper {
                           @Param("endL") String endL,
                           @Param("endR") String endR);
 
-    @Delete({"<script>",
-             "DELETE FROM employee WHERE id IN",
-             "<foreach collection='ids' item='id' open='(' separator=',' close=')'>",
-             "#{id}",
-             "</foreach>",
-             "</script>"})
-    void deleteByIds(@Param("ids") List<Long> ids);
 }
